4e64f0ea2ef8177a40b38fa8ea262ac4b1b4b621
SadrikA78 <men.arty@yandex.ru>
2020-09-21 23:57:12 +0300

    1
    1

--- uchet/creatQR.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-from key_generation import generate_key
-def return_qr(centr,section,data):
-    key = generate_key()
-    print('-'*20 + key)
-    img = qrcode.make(f'http://194.67.86.151:1111/?centr={centr}&section={section}&data={data}')
-    img.save(f'{key}.png')
-#return_qr(id,data)
-    return(f'{key}.png')
